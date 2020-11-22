ee89bde91618ea481054199172fd14da6902552a
Konstantin S <HavenDV@users.noreply.github.com>
2020-11-22 16:37:21 +1000

    Implemented ReplaceAll task.

--- src/H.Build.CustomTasks/Extensions/StringExtensions.cs
+++ src/H.Build.CustomTasks/Extensions/StringExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 #nullable enable
 
@@ -9,5 +9,53 @@ namespace H.Build.CustomTasks.Extensions
     /// </summary>
     public static class StringExtensions
     {
+        /// <summary>
+        /// Replaces the strings between the starting fragment and the ending.
+        /// All available fragments will be replaced.
+        /// <para/>Default <paramref name="comparison"/> is <see cref="StringComparison.Ordinal"/>.
+        /// <![CDATA[Version: 1.0.0.0]]>
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="value"></param>
+        /// <param name="count"></param>
+        /// <param name="comparison"></param>
+        /// <returns></returns>
+        public static string ReplaceAll(this string text, string start, string end, string value, out int count, StringComparison? comparison = null)
+        {
+            text = text ?? throw new ArgumentNullException(nameof(text));
+            start = start ?? throw new ArgumentNullException(nameof(start));
+            end = end ?? throw new ArgumentNullException(nameof(end));
+
+            count = 0;
+
+            var index2 = -end.Length;
+            while (true)
+            {
+                var index1 = text.IndexOf(start, index2 + end.Length, comparison ?? StringComparison.Ordinal);
+                if (index1 < 0)
+                {
+                    return text;
+                }
+
+                index1 += start.Length;
+                index2 = text.IndexOf(end, index1, comparison ?? StringComparison.Ordinal);
+                if (index2 < 0)
+                {
+                    return text;
+                }
+
+                count++;
+
+                index1 -= start.Length;
+                index2 += end.Length;
+
+                text = text.Remove(index1, index2 - index1);
+                text = text.Insert(index1, value);
+
+                index2 = index1 + value.Length;
+            }
+        }
     }
 }
---
+++ src/H.Build.CustomTasks/ReplaceAll.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using H.Build.CustomTasks.Extensions;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace H.Build.CustomTasks
+{
+    public class ReplaceAll : Task
+    {
+        #region Properties
+
+        public string? Path { get; set; }
+
+        public string? Start { get; set; }
+
+        public string? End { get; set; }
+
+        public string? Value { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public override bool Execute()
+        {
+            Path = Path ?? throw new ArgumentNullException(nameof(Path));
+            Start = Start ?? throw new ArgumentNullException(nameof(Start));
+            End = End ?? throw new ArgumentNullException(nameof(End));
+            Value = Value ?? throw new ArgumentNullException(nameof(Value));
+
+            File.WriteAllText(Path, File.ReadAllText(Path)
+                .ReplaceAll(Start, End, Value, out var count, StringComparison.OrdinalIgnoreCase));
+
+            Log.LogMessage(MessageImportance.High, $"{nameof(ReplaceAll)}: Removed {count} matches.");
+
+            return true;
+        }
+
+        #endregion
+    }
+}
