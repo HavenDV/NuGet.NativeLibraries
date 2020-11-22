0bd96e7d1f9bc18bfa451384898ad094abb7fbed
Konstantin S <HavenDV@users.noreply.github.com>
2020-11-22 15:26:46 +1000

    Added H.Build.CustomTasks project.

---
+++ src/H.Build.CustomTasks/RemoveAll.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace H.Build.CustomTasks
+{
+    public class RemoveAll : Task
+    {
+        #region Properties
+
+        public string Path { get; set; }
+
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public override bool Execute()
+        {
+            var count = 1;
+            Log.LogMessage(MessageImportance.High, $"{nameof(RemoveAll)}: Removed {count} matches.");
+
+            return true;
+        }
+
+        #endregion
+    }
+}
